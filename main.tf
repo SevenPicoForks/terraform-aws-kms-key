f6865b6be76e91a996b99a9cad9a650fb57145e5
Dave Raring <d@7pi.co>
2022-08-18 14:10:18 -0500

    using our Context

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_kms_key" "default" {
-  count                    = module.this.enabled ? 1 : 0
+  count                    = module.context.enabled ? 1 : 0
   deletion_window_in_days  = var.deletion_window_in_days
   enable_key_rotation      = var.enable_key_rotation
   policy                   = var.policy
-  tags                     = module.this.tags
+  tags                     = module.context.tags
   description              = var.description
   key_usage                = var.key_usage
   customer_master_key_spec = var.customer_master_key_spec
@@ -11,7 +11,7 @@ resource "aws_kms_key" "default" {
 }
 
 resource "aws_kms_alias" "default" {
-  count         = module.this.enabled ? 1 : 0
-  name          = coalesce(var.alias, format("alias/%v", module.this.id))
+  count         = module.context.enabled ? 1 : 0
+  name          = coalesce(var.alias, format("alias/%v", module.context.id))
   target_key_id = join("", aws_kms_key.default.*.id)
 }
